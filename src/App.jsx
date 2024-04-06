848e4c86f0422cf1f0b486b7b1f613a8dd88cd49
Leonardo Bora <95290010+leonardobora@users.noreply.github.com>
2024-04-06 20:49:06 +0000

    Simple settings on a portfolio template to try out a few funcionalities.

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Leonardo Bora",
+  title: "Engineering Intern at Furukawa Eletric LatAm",
+  email: "leonardo.bora@outlook.com",
+  gitHub: "github.com/leonardobora",
+  instagram: "instagram.com/leonardobora_/",
+  linkedIn: "inkedin.com/in/leonardobora/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "twitter.com/plugbora",
+  youTube: "youtube.com/channel/UCwt-LGT3SLisDV5D4qzZNGA",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -9,28 +9,17 @@ import React from "react";
 import arrowSvg from "../images/down-arrow.svg";
 import PropTypes from "prop-types";
 
-/**
- * Home background image
- *
- * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
- * represents what you see in that image.
- *
- *
- * Need an image? Check out https://unsplash.com to download a photo you
- * freely use on your site.
- */
-import image from "../images/woman-with-tablet.jpg";
+import image from "../images/meatjungle.jpeg";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText = "";
 
 const Home = ({ name, title }) => {
   return (
     <section id="home" className="min-height">
-      <img className="background" src={image} alt="" />
-      <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
+      <img className="background" src={image} alt="" style={{ filter: "blur(0px)" }} />
+      <div style={{ position: "absolute", top: "5rem", left: "50%", transform: "translateX(-50%)", width: "17rem", color: "#FFFFFF", textShadow: "2px 2px #000000" }}>
         <h1>{name}</h1>
-        <h2>{title}</h2>
+        <h2 style={{ whiteSpace: "pre-wrap", width: "15rem" }}>{title}</h2>
       </div>
       <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
         <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
